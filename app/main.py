a2518b35c7701230ced94722d1ff467318fd5365
Binliu Zhang <binliu.zhang@gmail.com>
2024-11-02 16:23:12 +0100

    respond to PING

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+    conn.send("+PONG\r\n")
+    # server_socket.sendall("+PONG\r\n")
+
 
 
 if __name__ == "__main__":
