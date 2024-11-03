f334dc488031a15b8bc4ea5b8601eeef1bc13e45
Binliu <binliu.zhang@gmail.com>
2024-11-03 22:01:32 +0100

    more concise

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    conn.send("+PONG\r\n".encode())
+    conn.send(b"+PONG\r\n")
 
 
 
