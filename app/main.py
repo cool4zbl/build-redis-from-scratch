2e2ce29d40eaa7a272658f9c2818bf01a42bde1d
Binliu <binliu.zhang@gmail.com>
2024-11-03 22:40:22 +0100

    handle multiple PING

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    conn.send(b"+PONG\r\n")
 
+    byte_received = 0
+    MSG_SIZE = min(4, len(b"PING\nPING"))
+    while byte_received < MSG_SIZE:
+        conn.recv(MSG_SIZE - byte_received)
+        conn.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
