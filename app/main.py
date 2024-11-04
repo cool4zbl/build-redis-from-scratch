69c0d3b8559a27458609a69a28bffa59df2335c6
Binliu <binliu.zhang@gmail.com>
2024-11-04 08:53:12 +0100

    handle concurrent

--- app/main.py
+++ app/main.py
@@ -6,14 +6,15 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-
     while True:
-        data = conn.recv(1024)
+        conn, addr = server_socket.accept() # wait for client
 
-        if data == b"*1\r\n$4\r\nPING\r\n":
-            conn.send(b"+PONG\r\n")
-
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            if data == b"*1\r\n$4\r\nPING\r\n":
+                conn.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
