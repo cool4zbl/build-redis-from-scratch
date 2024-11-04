c846d676b74156ba2d13f773669cfec5094c27be
Binliu <binliu.zhang@gmail.com>
2024-11-04 09:02:32 +0100

    use threadPoolExecutor to handle concurrent requests

--- app/main.py
+++ app/main.py
@@ -1,4 +1,16 @@
 import socket  # noqa: F401
+import concurrent.futures  # noqa: F401
+
+def handle_client(conn, addr):
+    print(f"Connection from {addr} opened.")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        if data == b"*1\r\n$4\r\nPING\r\n":
+            conn.send(b"+PONG\r\n")
+    conn.close()
+    print(f"Connection from {addr} closed.")
 
 
 def main():
@@ -6,17 +18,12 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        conn, addr = server_socket.accept() # wait for client
-        if conn:
-            print("Connection from: ", addr)
 
+    with concurrent.futures.ThreadPoolExecutor() as executor:
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            if data == b"*1\r\n$4\r\nPING\r\n":
-                conn.send(b"+PONG\r\n")
+            conn, addr = server_socket.accept() # wait for client
+            executor.submit(handle_client, conn, addr)
+
 
 if __name__ == "__main__":
     main()
