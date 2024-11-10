dcebf4bc0a14b5fac19184e72bae62826c5e6af4
Binliu <binliu.zhang@gmail.com>
2024-11-10 21:31:45 +0100

    use coroutines with asyncio instead of threads

--- app/main.py
+++ app/main.py
@@ -1,29 +1,36 @@
-import socket  # noqa: F401
-import concurrent.futures  # noqa: F401
+import asyncio
 
-def handle_client(conn, addr):
-    print(f"Connection from {addr} opened.")
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        if data == b"*1\r\n$4\r\nPING\r\n":
-            conn.send(b"+PONG\r\n")
-    conn.close()
-    print(f"Connection from {addr} closed.")
+async def handle_client(reader, writer):
+    addr = writer.get_extra_info("peername")
+    print(f"Connection from {addr}.")
 
+    try:
+        while True:
+            data = await reader.read(1024)
+            if not data:
+                break
+            print(f"Received {data.decode()!r} from {addr}.")
+            if data == b"*1\r\n$4\r\nPING\r\n":
+                response = b"+PONG\r\n"
+                writer.write(response)
+                await writer.drain()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        writer.close()
+        await writer.wait_closed()
+        print(f"Connection from {addr} closed.")
 
-def main():
+async def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server = await asyncio.start_server(handle_client, host="localhost", port=6379, reuse_port=True)
+    addr = server.sockets
+    print(f"Server started at {addr}")
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        while True:
-            conn, addr = server_socket.accept() # wait for client
-            executor.submit(handle_client, conn, addr)
-
+    async with server:
+        await server.serve_forever()
 
 if __name__ == "__main__":
     main()
